179f5163baad4b6e8e4151ac8812c46f8b0cb48c
taojy123 <taojy123@163.com>
2020-06-08 11:25:46 +0800

    add main

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 demo for https://github.com/the-benchmarker/web-frameworks/issues/2892
 
+```
+git clone https://github.com/toajy123/valval
+ln -s $(pwd)/valval ~/.vmodules/valval
+git clone https://github.com/toajy123/valval_demo
+cd valval_demo && v run main.v
+curl http://127.0.0.1:8012
+```
---
+++ main.v
@@ -0,0 +1,28 @@
+module main
+
+
+import valval
+import json
+
+
+fn index(req valval.Request) valval.Response {
+	return valval.response_ok('')
+}
+
+
+fn user(req valval.Request) valval.Response {
+	if req.method == 'POST' {
+		return valval.response_ok('')
+	}
+	id := req.get('id', '-1')
+	return valval.response_ok(id)
+}
+
+
+fn main() {
+	mut app := valval.new_app(true)
+	app.route('/', index)
+	app.route('/user', user)
+	valval.runserver(app, 8012)
+}
+
